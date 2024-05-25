e6ce0be9475fda93aacf507f1ef02585e7d917dd
NoofFahad <162987198+NoofFahad@users.noreply.github.com>
2024-05-25 08:54:02 -0700

    Update Feedback.php

--- Feedback.php
+++ Feedback.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer Feedback</title>
+    <title>Feedback</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
